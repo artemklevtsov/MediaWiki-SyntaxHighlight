9ba36293e981aa067cede37ba659ddaf88a23749
Artem A Klevtsov <a.a.klevtsov@gmail.com>
2014-02-22 19:42:55 +0700

    Small fixes Update README

--- SyntaxHighlight.body.php
+++ SyntaxHighlight.body.php
@@ -7,13 +7,17 @@ class SyntaxHighlight {
     }
 
     public static function wfSyntaxHighlight($text, array $args, Parser $parser, PPFrame $frame) {
+	$code_classes = '';
         if (isset($args['lang']) && $args['lang']) {
             $lang = $args['lang'];
             $lang = strtolower($lang);
-            $class = 'language-'.$lang;
+            $code_classes = 'language-'.$lang;
         }
+        if (isset($args['class']) && $args['class']) {
+	    $code_classes .= ' '.$args['class'];
+	}
         $text = rtrim($text);
-        return '<pre><code class="'.$class.'">'.$text.'</code></pre>';
+        return '<pre><code class="'.$code_classes.'">'.$text.'</code></pre>';
     }
 
     public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
--- SyntaxHighlight.php
+++ SyntaxHighlight.php
@@ -33,6 +33,6 @@ $wgHooks['BeforePageDisplay'][] = 'SyntaxHighlight::onBeforePageDisplay';
 $wgResourceModules['ext.SyntaxHighlight'] = array(
     'localBasePath' => dirname(__FILE__),
     'remoteExtPath' => 'SyntaxHighlight',
-    'styles' => array('highlight.js/src/styles/'.$wgSyntaxHighlightStyle.'.css', 'style.css'),
-    'scripts' => array('highlight.js/build/highlight.pack.js', 'init.js')
+    'styles' => array('highlight.js/src/styles/'.$wgSyntaxHighlightStyle.'.css', 'ext.SyntaxHighlight.css'),
+    'scripts' => array('highlight.js/build/highlight.pack.js', 'ext.SyntaxHighlight.js')
 );
